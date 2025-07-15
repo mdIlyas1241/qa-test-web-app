a771dc0ad21af629906d608b05651b33412d464d
ilyasuk2024@gmail.com <ilyasuk2024@gmail.com>
2025-07-16 00:18:17 +0100

    Joins

---
+++ W3schools/solutions.sql
@@ -0,0 +1,41 @@
+--Show all products in the Beverages category.
+--Hint: You’ll need to join Products and Categories.
+
+--select p.ProductID, p.ProductName , p.SupplierID , p.CategoryID, p.unit, p.Price, c.CategoryName FROM categories c
+--INNER JOIN products p 
+  --ON c.CategoryID  = p.CategoryID 
+ --where c.categoryname = 'Beverages'
+
+--List all orders along with the name of the customer who placed them.
+--Hint: Join Orders and Customers.
+ 
+select o.OrderID, o.CustomerID, o.EmployeeID,o.OrderDate,o.ShipperID, c.CustomerName from customers c 
+INNER JOIN orders o
+  ON c.CustomerID = O.CustomerID 
+  
+  --Show the product name and supplier name for each product.
+--Hint: Join Products and Suppliers.
+  
+  select p.ProductName, s.SupplierName from suppliers s 
+  join products p 
+  on s.SupplierID = p.SupplierID
+  
+  
+   --List the names of all customers who have placed at least one order.
+--Hint: Join Customers and Orders, and eliminate duplicates.
+  select DISTINCT c.CustomerName from customers c 
+  join orders o 
+  on c.CustomerID = o.CustomerID
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
